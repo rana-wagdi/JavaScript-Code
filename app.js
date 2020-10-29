8fa4059b7875221c608c1104d9a3321f31d94ac7
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-10-29 16:50:13 -0700

    function Constructors and prototype

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-//function Constructors
+//function Constructors and prototype
 
 function Person(firstname,lastname){
 
     console.log(this);
-    this.firstname=firstname;
-    this.lastname=lastname;
+    this.firstname = firstname;
+    this.lastname = lastname;
     console.log('This function is invoked.');
+}
+
+Person.prototype.getFullName = function() {
+    return this.firstname + ' '+ this.lastname;
 }
 
 var john = new Person('John','Doe');
@@ -13,3 +17,9 @@ console.log(john);
 
 var jane = new Person('Jane','Doe');
 console.log(jane);
+
+Person.prototype.getFormalFullName=function(){
+    return this.lastname+', '+this.firstname;
+}
+
+console.log(john.getFormalFullName());
