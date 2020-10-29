dc6598be0cffc3161f9fd97c4142b857d225fec1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-10-29 16:24:19 -0700

    function Constructors

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
-// functional programming
-function mapForEach(arr, fn){
-    var newArr=[];
-    for (var i=0; i<arr.length;i++){
-        newArr.push(fn(arr[i])
-        )
-    };
+//function Constructors
 
-    return newArr;
+function Person(firstname,lastname){
+
+    console.log(this);
+    this.firstname=firstname;
+    this.lastname=lastname;
+    console.log('This function is invoked.');
 }
 
-var arr1= [1,2,3];
-console.log(arr1);
+var john = new Person('John','Doe');
+console.log(john);
 
-var arr2= mapForEach(arr1,function(item){
-    return item * 2;
-});
-console.log(arr2);
-
-var arr3 = mapForEach(arr1,function(item){
-    return item > 2;
-})
-console.log(arr3);
-
-var checkPastLimit = function(limiter,item){
-    return item > limiter;
-
-}
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this,1));
-console.log(arr4);
-
-var checkPastLimitSimplified = function(limiter){
-    return function(limiter,item) {
-        return item > limiter;
-    }.bind(this,limiter);
-};
-
-var arr5 = mapForEach(arr1,checkPastLimitSimplified(2));
-console.log(arr5);
+var jane = new Person('Jane','Doe');
+console.log(jane);
