0202434d71922743fba6dd4407de691675fe9b94
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-10-29 17:21:01 -0700

    function Constructors

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 //function Constructors and prototype
 
-function Person(firstname,lastname){
-
-    console.log(this);
-    this.firstname = firstname;
-    this.lastname = lastname;
-    console.log('This function is invoked.');
+String.prototype.isLengthGreaterThan = function(limit){
+    return this.length > limit;
 }
+console.log("John".isLengthGreaterThan(3));
 
-Person.prototype.getFullName = function() {
-    return this.firstname + ' '+ this.lastname;
+Number.prototype.isPositive = function(){
+    return this > 0;
+    
 }
-
-var john = new Person('John','Doe');
-console.log(john);
-
-var jane = new Person('Jane','Doe');
-console.log(jane);
-
-Person.prototype.getFormalFullName=function(){
-    return this.lastname+', '+this.firstname;
-}
-
-console.log(john.getFormalFullName());
