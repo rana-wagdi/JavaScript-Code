3976a2142a0a41d37b8996c0dfcc13fd9edb1a0b
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-10-29 22:04:01 -0700

    typeof

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-//function Constructors and prototype
+//typeof
 
-String.prototype.isLengthGreaterThan = function(limit){
-    return this.length > limit;
-}
-console.log("John".isLengthGreaterThan(3));
+var a=3;
+console.log(typeof a);
 
-Number.prototype.isPositive = function(){
-    return this > 0;
-    
+var b = "Hello";
+console.log(typeof b);
+
+var c = {};
+console.log(typeof c);
+
+var d =[];
+console.log(typeof d); //weird!
+console.log(Object.prototype.toString.call(d));
+
+function Person(name){
+    this.name=name;
 }
+
+var e = new Person('Jane');
+console.log(typeof e);
+console.log(e instanceof Person);
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+var z = function() { };
+console.log(typeof z);
