9f2496b377e708b49418cc4e618b875a9611b18f
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-10-30 20:11:38 -0700

    Greetr

--- Greetr.js
+++ Greetr.js
@@ -4,7 +4,60 @@
         return new Greetr.init(firstName,lastName,language);
     }
 
-    Greetr.prototype={};
+    var supportedLangs=['en','es'];
+
+   var greetings = {
+        en:'Hello',
+        es:'Hola'
+    };
+
+   var formalGreetings={
+        en:'Greetings',
+        es:'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es:'Incio seasion'
+    };
+
+
+
+    Greetr.prototype={
+        fullName:function(){
+            return this.firstName+' '+this.lastName;
+
+        },
+        validate:function(){
+           if (supportedLangs.indexOf(this.language) === -1){
+             throw "Invalid Language";
+            }
+        },
+
+        greeting: function(){
+            return greetings[this.language]+' '+this.firstName+'!';
+
+        },
+        formalGreeting:function(){
+            return formalGreetings[this.language]+', '+this.firstName();
+        },
+        greet: function(formal) {
+            var msg ;
+
+            //if undefined or null it well be coerced to 'false'
+            if (formal){
+                msg=this.formalGreeting();
+            }
+            else{
+                msg = this.greeting()
+            }
+
+            if(console){
+                console.log(msg);
+            }
+            return this;
+        }
+    };
 
     Greetr.init = function(firstName,lastName,language){
 
@@ -17,6 +70,6 @@
     Greetr.init.prototype = Greetr.prototype;
 
     global.Greetr = global.G$ =Greetr;
-    
+
     
 }(window, jQuery));
