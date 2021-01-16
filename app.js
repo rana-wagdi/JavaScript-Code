3f5f62b05c2b19e4a8bca0bbfbca51989e9e08b1
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-16 15:23:04 -0800

    solve problem

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-function addUpTo(n) {
-    let total =0;
-    for(let i =1; i<= n;i++) {
-    total += i;
+function charCount(str){
+    //make object to return at end
+    var result = {};
+    //loop over string ,for each character..
+    for (var i=0 ; i < str.length; i++){
+        var char = str[i]
+         //if th char is a number /letter and is a key in object,add one to count
+        if(result[char] > 0){
+            result[char]++;
+            //if th char is a number / letter and not in a object , add it to object and set value to 1
+        }else{
+            result[char]=1;
+        };
+       
+    }
+    
+    //if character is something else (space, period, etc.)don't do anything
+    //return aobject at end
+    return result;
 }
-    return total;
-}
-console.log(addUpTo(6));
