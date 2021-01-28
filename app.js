94c1750951f5b66c3d14111d7273b0898e86c529
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-28 01:18:11 -0800

    var&alert

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-function charCount(str){
-    //make object to return at end
-    var result = {};
-    //loop over string ,for each character..
-    for (var i=0 ; i < str.length; i++){
-        var char = str[i]
-         //if th char is a number /letter and is a key in object,add one to count
-        if(result[char] > 0){
-            result[char]++;
-            //if th char is a number / letter and not in a object , add it to object and set value to 1
-        }else{
-            result[char]=1;
-        };
-       
-    }
-    
-    //if character is something else (space, period, etc.)don't do anything
-    //return aobject at end
-    return result;
-}
+var myName = "Rana";
+alert(myName);
