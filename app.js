0e69d10344d26a712d00516f7adcc85da29f08a3
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-28 20:33:10 -0800

    lower&upperCase

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
  var name =prompt("What is your name!");
 
 var firstChar = name.slice(0,1);
+var lastChar = name.slice(1,name.length);
 
 var upperCase=firstChar.toUpperCase();
-var capital=upperCase + name.slice(1,name.length);
-alert("Hello, "+capital);
+var lowerCase = lastChar.toLowerCase();
+
+
+var capital=upperCase + lowerCase;
+alert("Hello, "+capital+" "+"!");
