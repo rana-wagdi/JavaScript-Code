1eb6f408ce77c4ec6c86e94bea2b60396bafca95
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-01-31 04:35:19 -0800

    BMI calculator

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-function lifeInWeeks(age) {
-  /************Don't change the code above************/
+function bmiCalculator(weight, height) {
+  //The first parameter should be the weight and the second should be the height.
 
-  //Write your code here.
-  var daysRemaning = 90 - age;
+  var bmi = weight /Math.pow(height ,2);
 
-  var days = daysRemaning * 365;
-  var weeks = Math.floor(daysRemaning * 52);
-  var years = Math.floor(daysRemaning* 12);
-
-  console.log(
-    "You have " +
-      days +
-      " days, " +
-      weeks +
-      " weeks, and " +
-      years +
-      " months left."
-  );
-
-  /*************Don't change the code below**********/
+  return bmi;
 }
-lifeInWeeks(56);
+bmiCalculator(65, 1.8);
+
