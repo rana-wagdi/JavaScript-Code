48a03edcd63092d59105eb0a85e36a0588e3ba4c
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-01 02:39:22 -0800

    random

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-function bmiCalculator(weight, height) {
-  //The first parameter should be the weight and the second should be the height.
-
-  var bmi = weight /Math.pow(height ,2);
-
-  return bmi;
-}
- var bmi = bmiCalculator(65, 1.8);
-
-console.log(Math.floor(bmi));
+var n = Math.random()
+console.log(n);
