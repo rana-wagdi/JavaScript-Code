fdb75f65c958b5388c4964812285a3821206871d
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-01 05:20:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var n = Math.random()
-console.log(n);
+var namePerson1 = prompt("what is name person 1");
+var namePerson2 = prompt("what is the name person 2");
+var calculate = Math.random();
+calculate = calculate * 100;
+calculate =Math.floor(calculate +1);
+console.log(calculate);
