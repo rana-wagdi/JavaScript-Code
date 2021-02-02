4e3dffdc5e7e081afe68dd4c864d3e6ce615da48
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-02 03:27:31 -0800

    Calculator advanced

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-prompt("what is our name?");
-prompt("what is their name?");
-
-var loveScore=Math.random()*100;
-loveScore=Math.floor(loveScore+1);
-alert("Your love score is "+loveScore +"%");
+function bmiCalculator(weight, height) {
+  var bmi = weight / (height * height);
+  var interpretation = Math.random(bmi) * 60;
+  interpretation=Math.floor(interpretation+1);
+  if (interpretation < 18.5) {
+    console.log("your BMI is, " +Math.floor( interpretation)+ " so you ara underweight");
+  } 
+  if( interpretation >=18.6 && interpretation <= 24.9){
+      console.log("your BMI is "+ Math.floor( interpretation)+"so you have a normal weih")
+  }
+if (interpretation > 24.9) {
+  console.log("your BMI is, " + interpretation + " so you ara overwieght");
+} 
+  return interpretation;
+}
+bmiCalculator(10,20);
