d913bcbbc888750e9d18700d2703d18ac324854b
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-02 13:00:58 -0800

    Arrays

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-function bmiCalculator(weight, height) {
-  var bmi = weight / (height * height);
-  var interpretation = Math.random(bmi) * 60;
-  interpretation=Math.floor(interpretation+1);
-  if (interpretation < 18.5) {
-    console.log("your BMI is, " +Math.floor( interpretation)+ " so you ara underweight");
-  } 
-  if( interpretation >=18.6 && interpretation <= 24.9){
-      console.log("your BMI is "+ Math.floor( interpretation)+"so you have a normal weih")
-  }
-if (interpretation > 24.9) {
-  console.log("your BMI is, " + interpretation + " so you ara overwieght");
-} 
-  return interpretation;
+var yourName = prompt("wha is your name?");
+
+var guestList=["Angla","Jack","Ahmed","wagdi","Rana"];
+if (guestList.includes(yourName)){
+   alert("Welcome!");
+}else {
+    alert("Sorry, maybe next time")
 }
-bmiCalculator(10,20);
+
