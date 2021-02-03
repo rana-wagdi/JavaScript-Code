115986b94de431e6aa0b1e62afc5754d46f4891c
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-03 02:47:24 -0800

    array

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var output =[];
-var count = 1;
-function fizzBuzz(){
-          if (count % 3 === 0 && count % 5 === 0) {
-            output.push("fizzBuzz");
-          } else if (count % 3 === 0) {
-            output.push("fizz");
-          } else if (count % 5 === 0) {
-            output.push("Buzz");
-          } else {
-            output.push(count);
-          }  
-  count++;
+function whosPaying(names) {
+  /******Don't change the code above*******/
 
-    console.log(output);
+  //Write your code here.
+
+   names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+
+    var nameOnList= names[Math.floor(Math.random()*names.length)];
+   personBuyLunch = nameOnList + " is going to buy lunch today!";
+
+  return personBuyLunch;
+
+  /******Don't change the code below*******/
 }
+whosPaying();
