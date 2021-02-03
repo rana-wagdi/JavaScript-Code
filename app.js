9b87e208c7c96f52bb261d4226b9960dc85c909f
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-03 09:27:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-var output =[];
-var count = 100;
+ var output =[];
+// var count = 100;
 
 
-function fizzBuzz(){
-    for (count=100; count>=0;count--) {
-      console.log(count+"bottls of beer")
-      count--;
+// function fizzBuzz(){
+//     for (count=100; count>=0;count--) {
+//       console.log(count+"bottls of beer")
+//       count--;
      
+//     }
+//      console.log(output);
+// }
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    
+    n=n+1;
+    for ( n=0; n<=10;n++){
+        
+        
     }
-     console.log(output);
+    
+    console.log(output);
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
 }
